1bbfcde5210187380f8ad139dbd2319efa67287e
sjamescarter <stephenjamescarter@gmail.com>
2023-04-21 14:48:48 -0500

    commit tree aaa21aa199a77b4eade659cf65fadc7425c46e1e

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -16,4 +18,28 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.update(
+      comment: params[:comment], 
+      score: params[:score]
+    )
+    review.to_json
+  end
 end
